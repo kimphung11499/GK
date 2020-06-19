281ffee46e1a6f28986514cf64fdf19e98661fea
Phung <kimphung11499@gmail.com>
2020-06-19 23:54:20 +0700

    first

---
+++ giuaki/giuaki/Controllers/viewProduct.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using giuaki.Models;
+
+namespace giuaki.Controllers
+{
+    internal class viewProduct
+    {
+        public List<Category> categories { get; set; }
+        public Product products { get; set; }
+    }
+}
